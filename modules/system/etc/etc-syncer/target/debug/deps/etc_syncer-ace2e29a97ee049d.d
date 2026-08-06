f91f7af104a7412085ed08ff8481d21e05b775b8
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-08-07 00:16:28 +0900

    fix: remove target directory from git tracking and update .gitignore

--- modules/system/etc/etc-syncer/target/debug/deps/etc_syncer-ace2e29a97ee049d.d
+++
@@ -1,5 +0,0 @@
-/home/teto/Documents/nixproject/NEET_OS/etc-syncer/target/debug/deps/etc_syncer-ace2e29a97ee049d.d: src/main.rs
-
-/home/teto/Documents/nixproject/NEET_OS/etc-syncer/target/debug/deps/etc_syncer-ace2e29a97ee049d: src/main.rs
-
-src/main.rs:
