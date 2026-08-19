0d8a1317191934efef4036c3cb932f883aa6187d
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-08-19 18:58:02 +0900

    chore: Kernel Debug Settings

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NEET OS - A minimal s6/Rust based OS";
+
+  outputs = {self}: let
+    sources = import ./npins;
+    pkgs = import sources.nixpkgs {system = "x86_64-linux";};
+    lib = pkgs.lib;
+
+    # OSの評価
+    myOS = lib.evalModules {
+      specialArgs = {inherit pkgs lib;};
+      modules = [./configuration.nix];
+    };
+
+    # 評価結果の config.system.build.initrd を渡すだけ
+    runner = import ./nix/runner.nix {
+      inherit pkgs;
+      kernel = myOS.config.system.build.kernel;
+      initrd = myOS.config.system.build.initrd;
+    };
+  in {
+    debugConfig = myOS.config;
+
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${runner}/bin/run-vm";
+    };
+  };
+}
