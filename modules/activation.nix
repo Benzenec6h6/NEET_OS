c43a875493d52341003a25bb74378628fde5148f
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-01 17:39:18 +0900

    fix: Resolving Permission Issues

--- modules/activation.nix
+++ modules/activation.nix
@@ -4,7 +4,6 @@
   lib,
   ...
 }: {
-  # 1. オプションの宣言 (これがないと代入できない)
   options = {
     system.activationScripts = lib.mkOption {
       type = lib.types.attrsOf lib.types.lines;
@@ -17,22 +16,16 @@
     };
   };
 
-  # 2. オプションへの代入
-  config.system.activationScript = pkgs.writeTextFile {
-    name = "activate";
-    executable = true;
-    destination = "/bin/activate"; # どこに配置するか明示（任意）
-    text = ''
-      #!/bin/execlineb -P
-      # 1. Rustでファイル群を構築
-      foreground {
-        ${config.system.etc.bin}/bin/system-init
-        ${config.system.etc.package}
-        ${config.system.path}
-        ${config.boot.kernelPackages.kernel}
-      }
-      # 2. 必要に応じて here に追加の処理を書く
-      true
-    '';
+  config.system.activationScript = pkgs.replaceVarsWith {
+    src = ./activate.execline;
+    replacements = {
+      execline = "${config.environment.execline}";
+      systemInitBin = "${config.system.etc.bin}/bin/system-init";
+      etcPackage = "${config.system.etc.package}";
+      systemPath = "${config.system.path}";
+      kernelPath = "${config.boot.kernelPackages.kernel}";
+    };
+    isExecutable = true;
+    dontPatchShebangs = true;
   };
 }
