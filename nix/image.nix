ca3d41f91a9f1f0abcabb22dcd837a1ff11894e6
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-10 10:57:08 +0900

    fix: Layout Changes

--- nix/image.nix
+++ nix/image.nix
@@ -18,9 +18,15 @@
   '';
 in
   pkgs.runCommand "neet-os-disk-image" {
-    nativeBuildInputs = [pkgs.e2fsprogs];
+    nativeBuildInputs = [pkgs.btrfs-progs];
   } ''
+    # 1. イメージ内の構造を /nix/store に合わせる
+    mkdir -p ./staging/nix/store
+
+    # 2. ストアパスを /nix/store の下にコピー
+    cp -a ${rootfs}/* ./staging/nix/store/
+
     truncate -s 2G $out
-    # -d で rootfs (内部に /nix/store を含む) をディスクに流し込む
-    mkfs.ext4 -L NEET_OS -d ${rootfs} $out
+    # staging (中身は nix/store/...) をルートとして書き込む
+    mkfs.btrfs -L NEET_OS -r ./staging $out
   ''
