2c93bd25ad3d31b51adb15b40f9a83290fc15eed
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-10 10:49:49 +0900

    fix: Modified to work in conjunction with neet.security.enable

--- modules/security/polkit.nix
+++ modules/security/polkit.nix
@@ -4,13 +4,14 @@
   lib,
   ...
 }: {
-  # s6-rc サービスデータベース構築用
-  system.s6-rc.services.polkitd = {
-    type = "longrun";
-    dependencies = ["dbus"];
-    run = ''
-      #!/bin/execlineb -P
-      ${pkgs.polkit.out}/lib/polkit-1/polkitd --no-debug
-    '';
+  config = lib.mkIf config.neet.security.enable {
+    system.s6-rc.services.polkitd = {
+      type = "longrun";
+      dependencies = ["dbus"];
+      run = ''
+        #!/bin/execlineb -P
+        ${pkgs.polkit.out}/lib/polkit-1/polkitd --no-debug
+      '';
+    };
   };
 }
