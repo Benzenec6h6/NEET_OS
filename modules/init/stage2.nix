5b116c8d2dd82d421b8b4dcb4be17cf82457c78c
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-15 13:28:43 +0900

    refactor: Remove activation.nix and consolidate

---
+++ modules/init/stage2.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  stage2Init = pkgs.replaceVarsWith {
+    src = ./stage2.execline;
+    replacements = {
+      execline = "${config.environment.execline}";
+      systemPath = "${config.system.path}";
+      systemInitBin = "${config.system.etc.bin}/bin/system-init";
+      etcPackage = "${config.system.etc.package}";
+      kernelPath = "${config.system.modulesTree}";
+    };
+    isExecutable = true;
+  };
+in {
+  config = {
+    # system/default.nix と stage1.nix の両方から参照される
+    system.build.stage2Init = stage2Init;
+  };
+}
