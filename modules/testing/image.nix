0d6606ca8a0e42930d227df92629a8e2f16d5352
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-19 18:35:34 +0900

    fix: Fixed size to prevent mount rejection

--- modules/testing/image.nix
+++ modules/testing/image.nix
@@ -16,7 +16,6 @@
     ];
   };
 
-  # 1. rootfs の骨格を作成
   rootfs =
     pkgs.runCommand "rootfs-staging" {
       nativeBuildInputs = [pkgs.nix pkgs.bash];
@@ -25,43 +24,36 @@
       bash ${./populate-rootfs.sh}
     '';
 
-  # 2. 各パーティションサイズ
-  espSizeM = 512; # ESP (FAT32) のサイズ (512 MiB)
-  rootSizeM = 3584; # Root (Btrfs) のサイズ (3.5 GiB)
-  # 全体サイズ: 先頭 1MB (GPTヘッダ) + ESP + Root + 末尾 1MB (GPT予備ヘッダ)
+  # ★サイズを MiB 単位で定義
+  espSizeM = 512; # ESP (FAT32): 512 MiB
+  rootSizeM = 3584; # Root (Btrfs): 3584 MiB (3.5 GiB)
+  # 全体: 先頭1MB + ESP(512MB) + Root(3584MB) + 末尾バックアップGPT用(1MB)
   diskSizeM = 1 + espSizeM + rootSizeM + 1;
 
   diskImage =
     pkgs.runCommand "neet-os-disk-image" {
       nativeBuildInputs = [
-        pkgs.gptfdisk # sgdisk
-        pkgs.dosfstools # mkfs.vfat
-        pkgs.mtools # mcopy, mmd (マウントなしでFATを操作するツール)
-        pkgs.btrfs-progs # mkfs.btrfs
-        pkgs.e2fsprogs # mkfs.ext4
-        pkgs.limine # BOOTX64.EFI
+        pkgs.gptfdisk
+        pkgs.dosfstools
+        pkgs.mtools
+        pkgs.btrfs-progs
+        pkgs.e2fsprogs
+        pkgs.limine
       ];
     } ''
-      # -------------------------------------------------------------
-      # A. ESP (FAT32) イメージの生成
-      # -------------------------------------------------------------
+      # 1. ESP (FAT32) イメージの作成
       echo "Creating ESP image..."
       truncate -s ${toString espSizeM}M esp.img
       mkfs.vfat -F 32 -n NEET_BOOT esp.img
 
-      # ディレクトリ作成
       mmd -i esp.img ::/EFI
       mmd -i esp.img ::/EFI/BOOT
       mmd -i esp.img ::/kernels
 
-      # Limine UEFI バイナリの配置
       mcopy -i esp.img ${pkgs.limine}/share/limine/BOOTX64.EFI ::/EFI/BOOT/BOOTX64.EFI
-
-      # 第1世代のカーネルと initrd を配置
       mcopy -i esp.img ${toplevel}/kernel ::/kernels/gen-1-vmlinuz
       mcopy -i esp.img ${toplevel}/initrd ::/kernels/gen-1-initrd
 
-      # 初期 limine.conf の作成と配置
       cat <<EOF > limine.conf
       timeout: 5
 
@@ -73,36 +65,32 @@
       EOF
       mcopy -i esp.img limine.conf ::/limine.conf
 
-      # -------------------------------------------------------------
-      # B. RootFS (Btrfs/ext4) イメージの生成
-      # -------------------------------------------------------------
+      # 2. RootFS (Btrfs) イメージの作成
       echo "Creating RootFS image (${rootFsType})..."
       truncate -s ${toString rootSizeM}M rootfs.img
+
+      # ★超重要: -b (バイトサイズ) を指定して、mkfs が勝手にサイズを拡張するのを防ぐ
       ${
         if rootFsType == "btrfs"
-        then "mkfs.btrfs -L NEET_OS -r ${rootfs} rootfs.img"
+        then "mkfs.btrfs -b ${toString (rootSizeM * 1024 * 1024)} -L NEET_OS -r ${rootfs} rootfs.img"
         else if rootFsType == "ext4"
-        then "mkfs.ext4 -L NEET_OS -d ${rootfs} rootfs.img"
+        then "mkfs.ext4 -L NEET_OS -d ${rootfs} rootfs.img ${toString rootSizeM}M"
         else throw "未対応の fsType です"
       }
 
-      # -------------------------------------------------------------
-      # C. 全体ディスクイメージの組み立て (GPT パーティション)
-      # -------------------------------------------------------------
-      echo "Assembling final GPT disk image..."
+      # 3. GPT ディスクの構築
+      echo "Assembling GPT disk image..."
       truncate -s ${toString diskSizeM}M $out
 
-      # 1MB 境界に合わせたセクタ指定 (1 sector = 512 bytes)
-      # 1MB = 2048 sectors
-      # 512MB = 1048576 sectors
+      # GPT テーブル初期化 & パーティション作成
       sgdisk -Z $out
+      # p1: 1MB(2048セクタ) から 512MB
       sgdisk -n 1:2048:+${toString espSizeM}M -t 1:ef00 -c 1:"EFI" $out
-      sgdisk -n 2:0:0                        -t 2:8300 -c 2:"root" $out
+      # p2: 513MB から 3584MB
+      sgdisk -n 2:${toString ((1 + espSizeM) * 2048)}:+${toString rootSizeM}M -t 2:8300 -c 2:"root" $out
 
-      # データを所定のオフセットに書き込み (conv=notrunc で上書き)
-      # 先頭 1MB (seek=1) に ESP を配置
+      # 結合 (conv=notrunc)
       dd if=esp.img of=$out bs=1M seek=1 conv=notrunc status=none
-      # 1MB + 512MB = 513MB (seek=513) に RootFS を配置
       dd if=rootfs.img of=$out bs=1M seek=${toString (1 + espSizeM)} conv=notrunc status=none
 
       echo "GPT disk image successfully created!"
