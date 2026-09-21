f99bc8cb5bff88f81b9072b0bf62e84872b03835
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-21 17:17:00 +0900

    fix: Verification of Deadlock Avoidance

--- modules/system/etc/inieet/system-init/src/shutdown.rs
+++ modules/system/etc/inieet/system-init/src/shutdown.rs
@@ -17,7 +17,7 @@ pub fn do_shutdown(action: Action) -> io::Result<()> {
 
     // 1. s6-rc で稼働中の全サービスを停止
     println!("system-init: stopping services via s6-rc...");
-    let _ = Command::new("s6-rc").args(["-a", "-da", "change"]).status();
+    // let _ = Command::new("s6-rc").args(["-a", "-da", "change"]).status();
 
     // 2. ディスクのキャッシュをフラッシュ
     println!("system-init: syncing disks...");
