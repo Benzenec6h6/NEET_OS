21f688d64b0a5824849a4813b8b9be4e418a0953
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-22 18:06:13 +0900

    feat: Specify a generic driver

--- modules/boot/kernel.nix
+++ modules/boot/kernel.nix
@@ -44,5 +44,55 @@
 
     # 標準でロードしておくべきモジュール
     boot.kernelModules = lib.mkDefault ["loop" "atkbd"];
+
+    boot.initrd.availableKernelModules =
+      [
+        # SATA/PATA
+        "ahci"
+        "sata_nv"
+        "sata_via"
+        "sata_sis"
+        "sata_uli"
+        "ata_piix"
+        "pata_marvell"
+
+        # NVMe
+        "nvme"
+
+        # SCSI（SATAディスクやUSBストレージの認識に必須）
+        "sd_mod"
+        "sr_mod"
+
+        # SDカード / eMMC
+        "mmc_block"
+
+        # USB ホストコントローラ（USB 1.1 / 2.0 / 3.x）
+        "uhci_hcd"
+        "ehci_hcd"
+        "ehci_pci"
+        "ohci_hcd"
+        "ohci_pci"
+        "xhci_hcd"
+        "xhci_pci"
+
+        # キーボード・入力デバイス
+        "usbhid"
+        "hid_generic"
+        "hid_lenovo"
+        "hid_apple"
+        "hid_roccat"
+        "hid_logitech_hidpp"
+        "hid_logitech_dj"
+        "hid_microsoft"
+        "hid_cherry"
+        "hid_corsair"
+      ]
+      ++ lib.optionals pkgs.stdenv.hostPlatform.isx86 [
+        # x86 のキーボード / RTC
+        "pcips2"
+        "atkbd"
+        "i8042"
+        "rtc_cmos"
+      ];
   };
 }
