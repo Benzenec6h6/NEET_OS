36e8ac320e37776a2ef0587f33c17d30c137a5a2
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-25 17:11:01 +0900

    fix: Setting Shell Environment Variables

--- modules/system/shells.nix
+++ modules/system/shells.nix
@@ -14,8 +14,6 @@
     if lib.isPath v
     then "${v}"
     else toString v;
-
-  # パッケージなら実行ファイルのパス (lib.getExe) を、文字列やパスならそのまま文字列化
   toShellPath = x:
     if lib.isDerivation x
     then lib.getExe x
@@ -27,15 +25,12 @@
     )
   );
 
-  # 重複を排除したシェルリスト
   allShells = lib.unique ((map toShellPath config.environment.shells) ++ ["/bin/sh"]);
 in {
   options.environment.shells = lib.mkOption {
-    # package, path, str のいずれも受け付けるように柔軟化
     type = with lib.types; listOf (oneOf [package path str]);
     default = [];
     description = "List of allowed login shells (/etc/shells).";
-    example = lib.literalExpression "[ pkgs.bashInteractive \"/bin/sh\" ]";
   };
 
   options.environment.variables = lib.mkOption {
@@ -53,12 +48,10 @@ in {
   };
 
   config = {
-    # 1. 重複のないクリーンな /etc/shells を生成
     environment.etc."shells".text = ''
       ${lib.concatStringsSep "\n" allShells}
     '';
 
-    # 2. 親となる /etc/profile は「profile.d を読み込むだけ」のシンプルな存在に徹する
     environment.etc."profile".text = ''
       # /etc/profile: system-wide initialisation for POSIX login shells
       if [ -d /etc/profile.d ]; then
@@ -69,7 +62,6 @@ in {
       fi
     '';
 
-    # 3. 定義された環境変数を session-vars.sh に書き出す
     environment.etc."profile.d/session-vars.sh" = lib.mkIf (config.environment.variables != {}) {
       text = sessionVarsScript;
     };
