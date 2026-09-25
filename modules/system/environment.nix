63df0d0698a9d94fcdab4dbec1b39bc69bf53d9f
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-09-25 16:30:10 +0900

    feat: Landmine Countermeasures

--- modules/system/environment.nix
+++ modules/system/environment.nix
@@ -4,7 +4,6 @@
   config,
   ...
 }: let
-  # rebuild スクリプトをパッケージ化
   neetRebuild = pkgs.writeShellScriptBin "neet-rebuild" (builtins.readFile ./rebuild.sh);
 in {
   options = {
@@ -12,15 +11,17 @@ in {
       type = lib.types.listOf lib.types.package;
       default = [];
     };
-    environment.execline = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.execline;
-      description = ''
-        execline package. /bin/execlineb from this package is relied upon,
-        by literal absolute path, as the interpreter for PID 1's init script
-        and the system activation script.
-      '';
+
+    environment.pathsToLink = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "/bin"
+        "/sbin"
+        "/share/vulkan"
+      ];
+      description = "Directories to be symlinked in system-path";
     };
+
     system.path = lib.mkOption {
       internal = true;
       type = lib.types.path;
@@ -28,11 +29,19 @@ in {
   };
 
   config = {
-    # ★ neetRebuild を追加
-    environment.systemPackages = [
-      config.environment.execline
+    # 必須の基幹ツール群を標準で含める
+    environment.systemPackages = with pkgs; [
       neetRebuild
-      pkgs.util-linux
+      config.environment.execline
+
+      # 基本の UNIX ツール群
+      util-linux
+      findutils
+      gnugrep
+      gawk
+      procps
+      which
+      less
     ];
 
     system.path = pkgs.buildEnv {
@@ -48,31 +57,34 @@ in {
             ''}
           '')
         ];
-      pathsToLink = [
-        "/bin"
-        "/graphics-drivers"
-        "/graphics-drivers-32bit"
-      ];
+      pathsToLink = config.environment.pathsToLink;
       ignoreCollisions = true;
+
+      postBuild = ''
+        # タブ補完を汚すラッパーバイナリを消去
+        find $out/bin -maxdepth 1 -name ".*-wrapped" -type l -delete
+
+        # GSettings / GLib スキーマのコンパイル
+        if [ -x $out/bin/glib-compile-schemas -a -w $out/share/glib-2.0/schemas ]; then
+          $out/bin/glib-compile-schemas $out/share/glib-2.0/schemas
+        fi
+      '';
     };
+
+    # 最重要：名前解決とユーザー解決の基盤
+    environment.etc."nsswitch.conf".text = ''
+      passwd:         files
+      group:          files
+      shadow:         files
+
+      hosts:          files dns
+      networks:       files
+    '';
 
     environment.etc."os-release".text = ''
       NAME="NEET OS"
       ID=neet-os
       PRETTY_NAME="NEET OS v0.1"
-    '';
-
-    # /etc/profile を強化 (環境変数の一元化)
-    environment.etc."profile".text = ''
-      export HOME=''${HOME:-/root}
-      export PATH=/run/wrappers/bin:/run/current-system/bin:/bin:/sbin
-      export TERM=linux
-      export PS1='\e[1;32mNEET-OS\e[0m \w \$ '
-
-      # Nix 関連
-      export NIX_REMOTE=daemon
-      export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-      export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
     '';
   };
 }
